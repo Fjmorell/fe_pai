0fb82cd7ff2e1d3c6a69b618985ddc45a41cb780
gbossero <guillermo.bossero@blink-srl.com>
2025-06-16 18:47:49 +0000

    Ajuste Typescript v0.2

--- src/infrastructure/authProvider.ts
+++ src/infrastructure/authProvider.ts
@@ -1,17 +1,22 @@
+type LoginParams = {
+  username: string;
+  password: string;
+};
+
 const authProvider = {
-  login: ({ username, password }) => {
+  login: async ({ username, password }: LoginParams) => {
     if (username && password) {
-      localStorage.setItem('auth', username);
+      localStorage.setItem("auth", username);
       return Promise.resolve();
     }
     return Promise.reject();
   },
   logout: () => {
-    localStorage.removeItem('auth');
+    localStorage.removeItem("auth");
     return Promise.resolve();
   },
   checkAuth: () => {
-    return localStorage.getItem('auth') ? Promise.resolve() : Promise.reject();
+    return localStorage.getItem("auth") ? Promise.resolve() : Promise.reject();
   },
   checkError: () => Promise.resolve(),
   getPermissions: () => Promise.resolve(),
